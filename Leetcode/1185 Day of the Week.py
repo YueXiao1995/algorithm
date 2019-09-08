e187120d64f7361111f7f57f8f4f77d2c8857276
yue <1995yuexiao@gmail.com>
2019-09-08 13:11:23 +0100

    problem 643

--- Leetcode/1185 Day of the Week.py
+++ Leetcode/1185 Day of the Week.py
@@ -22,32 +22,40 @@ Constraints:
     The given dates are valid dates between the years 1971 and 2100.
 """
 
-import datetime
 def dayOfTheWeek(day, month, year):
     # the 1971, 1, 1 is Friday
+    # for a given date, calculate the number of days between that day and 1997,1,1
     days = 0
+    # first, add the total number of days between last year and 1997 to the days variable
     for i in range(1971, year):
-        # if the year is the leap year
-        if i % 4 == 0:
+        # if the year is the leap year, the year has 366 day
+        if (i % 4 == 0 and i % 100 != 0) or i % 400 == 0:
             days += 366
         else:
             days += 365
+
+    # the number of days in each month
     days_by_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+    # add the number of days from january to last month
     for i in range(0, month - 1):
         days += days_by_month[i]
 
+    # if it is a leap year and the target month later than february, plus one
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        # in leap year february has 29 days
         if month > 2:
-            days += 1 # in leap year february has 29 days
+            days += 1
 
+    # add the number of days in this month
     days += day
 
+    # because 1971,1,1 already has one day, we need minus one
     days -= 1
 
-    print(days%7)
-
-    n = days%7
+    # because the 1971,1,1 is friday, so we can use the number of days between target date and that day,
+    # to get the corresponding day of the week for that date.
+    n = days % 7
     if n == 0:
         return "Friday"
     elif n == 1:
@@ -65,5 +73,3 @@ def dayOfTheWeek(day, month, year):
 
 
 print(dayOfTheWeek(8, 9, 2019))
-
-print(datetime.datetime(2019, 9, 8).strftime("%w"))
--- Leetcode/643 Maximum Average Subarray I.py
+++ Leetcode/643 Maximum Average Subarray I.py
@@ -11,3 +11,28 @@ Note:
     1 <= k <= n <= 30,000.
     Elements of the given array will be in the range [-10,000, 10,000].
 """
+def findMaxAverage(nums, k):
+    max_sum = None
+    temp_sum = None
+    last_num = None
+
+    for i in range(0, len(nums) - k + 1):
+        if max_sum == None:
+            sum = 0
+            for j in range(0, k):
+                sum += nums[j]
+            max_sum = sum
+            temp_sum = sum
+            last_num = nums[i]
+        else:
+            temp_sum -= last_num
+            temp_sum += nums[i + k - 1]
+            last_num = nums[i]
+            if temp_sum > max_sum:
+                max_sum = temp_sum
+
+    return float(max_sum)/ k
+
+input1 = [1,12,-5,-6,50,3]
+k1 = 4
+print(findMaxAverage(input1, k1))
