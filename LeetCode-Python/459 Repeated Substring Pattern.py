eb66feabe78caf316a78b050404ce87c9a6df147
yue <1995yuexiao@gmail.com>
2019-09-30 15:46:17 +0100

    9.30

--- LeetCode-Python/459 Repeated Substring Pattern.py
+++ LeetCode-Python/459 Repeated Substring Pattern.py
@@ -17,10 +17,30 @@ Example 3:
 """
 
 def repeatedSubstringPattern(s: str) -> bool:
-    return False
+    can_be_construced = False
+    l_s = len(s)
+    for i in range(0, l_s // 2 + 1):
+
+        if l_s % (i + 1) == 0:
+
+            substring = s[:i + 1]
+            l_sub_s = len(substring)
+            index = l_sub_s
+
+            while index <= l_s - l_sub_s:
+                if s[index:index + l_sub_s] != substring:
+                    break
+
+                if index == l_s - l_sub_s:
+                    can_be_construced = True
+                    break
+                else:
+                    index += l_sub_s
+    return can_be_construced
 
 input1 = "abab"
 input2 = "aba"
 input3 = "abcabcabcabc"
+input4 = "a"
 
-print(repeatedSubstringPattern(input1))
+print(repeatedSubstringPattern(input4))
