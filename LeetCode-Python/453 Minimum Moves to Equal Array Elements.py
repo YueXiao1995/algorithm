03265e401a6997551ade5c4615e02882a04c877f
yue <1995yuexiao@gmail.com>
2019-10-28 22:49:45 +0000

    10. 27

--- LeetCode-Python/453 Minimum Moves to Equal Array Elements.py
+++ LeetCode-Python/453 Minimum Moves to Equal Array Elements.py
@@ -16,94 +16,46 @@ Only three moves are needed (remember each move increments two elements):
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 """
 
-"""
-def minMoves(nums):
-    l = len(nums)
-    min = nums[0]
-    min_index = 0
-    max = nums[0]
-    max_index = 0
-    index = 0
-    while index < l:
-        if nums[index] > max:
-            max_index = index
-            max = nums[index]
-        elif nums[index] < min:
-            min_index = index
-            min = nums[index]
-        index += 1
-    print(min)
-    print(max)
+# Time Limit Exceeded
+def minMoves2(nums):
 
-    num_of_move = 0
-    while min != max:
-        num_of_move += max - min
-        for i in range(0, l):
-            if i != max_index:
-                nums[i] += max - min
-        index = 0
-        min = nums[0]
-        min_index = 0
-        max = nums[0]
-        max_index = 0
-        while index < l:
-            if nums[index] > max:
-                max_index = index
-                max = nums[index]
-            elif nums[index] < min:
-                min_index = index
-                min = nums[index]
-            index += 1
+    def areElementsEqual(nums):
+        first_element = nums[0]
+        for num in nums:
+            if num != first_element:
+                return False
+        return True
 
-    return num_of_move
-"""
-def minMoves(nums):
-    l = len(nums)
-    min = nums[0]
-    max = nums[0]
-    max_index = 0
-    index = 0
+    def findMinDiffToMaxValue(nums):
+        l = len(nums)
+        max_value = nums[0]
+        max_value_index = 0
+        for i in range(l):
+            if nums[i] > max_value:
+                max_value = nums[i]
+                max_value_index = i
+        i = 0
+        while i < l:
+            if nums[i] == max_value:
+                del nums[i]
+                l -= 1
+            else:
+                i += 1
+        second_max_value = max(nums)
+        return max_value - second_max_value, max_value_index
 
-    while index < l:
-        if nums[index] > max:
-            max = nums[index]
-            max_index = index
-        elif nums[index] < min:
-            min = nums[index]
-        index += 1
-    print(min)
-    print(max)
-
-    all_equal_except_max = True
-    index = 0
-    while index < l:
-        if index != max_index and nums[index] != min:
-            all_equal_except_max = False
-        index += 1
-
-
-    diff_sum = 0
-    index = 0
-    while index < l:
-        diff_sum += max - nums[index]
-        index += 1
-
-    if diff_sum == 0:
-        return 0
-    print(diff_sum)
-    if all_equal_except_max == False:
-        diff_sum += l
-
-    while diff_sum % (l - 1) != 0:
-        diff_sum += l
-
-    return diff_sum // (l - 1)
-
-
-
+    num_of_moves = 0
+    while areElementsEqual(nums) == False:
+        min_diff, max_value_index = findMinDiffToMaxValue(list(nums))
+        nums = [x + min_diff for x in nums]
+        nums[max_value_index] -= min_diff
+        num_of_moves += min_diff
+        print(nums)
+    return num_of_moves
 
 input1 = [1, 2, 3]
 input2 = [0, 0, 0]
 input3 = [1, 2]
 input4 = [-100, 0, 100]
-print(minMoves(input4))
+input5 = [-1, 1]
+print(minMoves2(input5))
