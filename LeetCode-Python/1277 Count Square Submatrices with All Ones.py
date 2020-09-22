14d841514e21fd2e9ee1f6d05d4bb7b0423f2d59
yue <1995yuexiao@gmail.com>
2020-09-23 02:10:46 +0800

    problem 1277

--- LeetCode-Python/1277 Count Square Submatrices with All Ones.py
+++ LeetCode-Python/1277 Count Square Submatrices with All Ones.py
@@ -37,8 +37,40 @@ Constraints:
 
 
 def countSquares(matrix):
-
-    return 0
+    h = len(matrix)
+    w = len(matrix[0])
+    max_size = min(h, w)
+    valid_points = set()
+    valid_submatrices = 0
+    for i in range(h):
+        for j in range(w):
+            if matrix[i][j] == 1:
+                valid_points.add((i, j))
+                valid_submatrices += 1
+    size = 2
+    while size <= max_size:
+        new_valid_points = set()
+        for point in valid_points:
+            points = set()
+            is_point_valid = True
+            if point[0] < h - size + 1 and point[1] < w - size + 1:
+                for i in range(point[0], point[0] + size):
+                    for j in range(point[1], point[1] + size):
+                        points.add(point)
+                        if matrix[i][j] != 1:
+                            is_point_valid = False
+                            break
+            else:
+                is_point_valid = False
+            if is_point_valid:
+                valid_submatrices += 1
+                new_valid_points = new_valid_points | points
+        size += 1
+        if len(new_valid_points) == 0:
+            break
+        else:
+            valid_points = new_valid_points
+    return valid_submatrices
 
 matrix =[
       [0,1,1,1],
